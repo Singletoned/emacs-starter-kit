557538a9b8f51d2c47de7c0dded1baac11cacff9
Ed Singleton <singletoned@gmail.com>
2009-05-27 15:05:16 +0100

    Added rectangle shortcuts

--- singletoned.el
+++ singletoned.el
@@ -3,3 +3,19 @@
 (global-set-key (kbd "M-p") 'backward-paragraph)
 (global-set-key (kbd "M-n") 'forward-paragraph)
 
+;; Support for marking a rectangle of text with highlighting.
+(global-set-key (kbd "C-x r C-@") 'rm-set-mark)
+(global-set-key (kbd "C-x r C-SPC") 'rm-set-mark)
+(global-set-key (kbd "C-x r C-x") 'rm-exchange-point-and-mark)
+(global-set-key (kbd "C-x r C-k") 'rm-kill-region)
+(global-set-key (kbd "C-x r A-x") 'rm-kill-region)
+(global-set-key (kbd "C-x r A-c") 'rm-kill-ring-save)
+(autoload 'rm-set-mark "rect-mark"
+  "Set mark for rectangle." t)
+(autoload 'rm-exchange-point-and-mark "rect-mark"
+  "Exchange point and mark for rectangle." t)
+(autoload 'rm-kill-region "rect-mark"
+  "Kill a rectangular region and save it in the kill ring." t)
+(autoload 'rm-kill-ring-save "rect-mark"
+  "Copy a rectangular region to the kill ring." t)
+
