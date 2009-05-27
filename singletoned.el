85e5758dcfe3f7a28539761e639a6582b8c034e7
Ed Singleton <singletoned@gmail.com>
2009-05-27 15:48:04 +0100

    Added some skeleton pair stuff based on cmarcelos blog post

--- singletoned.el
+++ singletoned.el
@@ -11,6 +11,41 @@
 (global-set-key (kbd "s-r") 'revbufs)
 (global-set-key (kbd "s-C-<return>") 'break-and-indent-brackets)
 
+;; Some lovely skeleton stuff, courtesy of cmarcelo
+;; http://cmarcelo.wordpress.com/2008/04/26/a-little-emacs-experiment/
+
+(defvar my-skeleton-pair-alist
+  '((?\) . ?\()
+    (?\] . ?\[)
+    (?} . ?{)
+    (?" . ?")))
+
+(defun my-skeleton-pair-end (arg)
+  "Skip the char if it is an ending, otherwise insert it."
+  (interactive "*p")
+  (let ((char last-command-char))
+    (if (and (assq char my-skeleton-pair-alist)
+             (eq char (following-char)))
+        (forward-char)
+      (self-insert-command (prefix-numeric-value arg)))))
+
+(defadvice backward-delete-char-untabify
+  (before my-skeleton-backspace activate)
+  "When deleting the beginning of a pair, and the ending is next char, delete it too."
+  (let ((pair (assq (following-char) my-skeleton-pair-alist)))
+    (and pair
+         (eq (preceding-char) (rest pair))
+         (delete-char 1))))
+
+(dolist (pair my-skeleton-pair-alist)
+  (global-set-key (char-to-string (first pair))
+                  'my-skeleton-pair-end)
+  ;; If the char for begin and end is the same,
+  ;; use the original skeleton
+  (global-set-key (char-to-string (rest pair))
+                  'skeleton-pair-insert-maybe))
+
+
 ;; Support for marking a rectangle of text with highlighting.
 (global-set-key (kbd "C-x r C-@") 'rm-set-mark)
 (global-set-key (kbd "C-x r C-SPC") 'rm-set-mark)
